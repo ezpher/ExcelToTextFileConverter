87d196e0f5c2adcebb591c20133432f4bd7a1f48
Hong Dah Ong <40519064+ezpher@users.noreply.github.com>
2020-10-25 15:01:21 +0800

    hide blinking ibeam cursor on focus of the readonly textbox

--- ExcelToTextConvertor/Form1.cs
+++ ExcelToTextConvertor/Form1.cs
@@ -6,6 +6,7 @@ using System.Data;
 using System.Drawing;
 using System.IO;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -15,6 +16,13 @@ namespace ExcelToTextConvertor
     public partial class Form1 : Form
     {
         private DataTableCollection dataTableCollection;
+
+        [DllImport("user32.dll")]
+        static extern bool HideCaret(System.IntPtr hWnd);
+        public void HideCaret()
+        {
+            HideCaret(readonlyUploadExcel.Handle);
+        }
 
         public Form1()
         {
@@ -64,6 +72,12 @@ namespace ExcelToTextConvertor
                     }
                 }
             }
+        }
+
+        // hide blinking ibeam cursor on focus of the readonly textbox
+        private void readonlyUploadExcel_GotFocus(object sender, EventArgs e)
+        {
+            HideCaret();
         }
     }
 }
