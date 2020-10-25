a0fbbc3235fbd49fc55d84aea4a4c5454149d00f
Hong Dah Ong <40519064+ezpher@users.noreply.github.com>
2020-10-25 20:14:04 +0800

    Handle Exception when using select column header mode Add Text file export Functionality Add TODO List

--- ExcelToTextConvertor/Form1.Designer.cs
+++ ExcelToTextConvertor/Form1.Designer.cs
@@ -34,21 +34,30 @@
             this.btnUploadExcel = new System.Windows.Forms.Button();
             this.comboBoxExcelSheet = new System.Windows.Forms.ComboBox();
             this.lblExcelSheet = new System.Windows.Forms.Label();
+            this.backgroundWorkerTextFileExport = new System.ComponentModel.BackgroundWorker();
+            this.lblTextFileExport = new System.Windows.Forms.Label();
+            this.readonlyTextFileExport = new System.Windows.Forms.TextBox();
+            this.btnTextFileExport = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridViewExcel)).BeginInit();
             this.SuspendLayout();
             // 
             // dataGridViewExcel
             // 
+            this.dataGridViewExcel.AllowUserToAddRows = false;
+            this.dataGridViewExcel.AllowUserToDeleteRows = false;
             this.dataGridViewExcel.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridViewExcel.Location = new System.Drawing.Point(12, 12);
+            this.dataGridViewExcel.Location = new System.Drawing.Point(15, 12);
             this.dataGridViewExcel.Name = "dataGridViewExcel";
-            this.dataGridViewExcel.Size = new System.Drawing.Size(741, 298);
+            this.dataGridViewExcel.ReadOnly = true;
+            this.dataGridViewExcel.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.ColumnHeaderSelect;
+            this.dataGridViewExcel.Size = new System.Drawing.Size(738, 245);
             this.dataGridViewExcel.TabIndex = 0;
+            this.dataGridViewExcel.DataSourceChanged += new System.EventHandler(this.dataGridViewExcel_DataSourceChanged);
             // 
             // lblUploadExcel
             // 
             this.lblUploadExcel.AutoSize = true;
-            this.lblUploadExcel.Location = new System.Drawing.Point(12, 335);
+            this.lblUploadExcel.Location = new System.Drawing.Point(13, 308);
             this.lblUploadExcel.Name = "lblUploadExcel";
             this.lblUploadExcel.Size = new System.Drawing.Size(55, 13);
             this.lblUploadExcel.TabIndex = 1;
@@ -57,16 +66,15 @@
             // readonlyUploadExcel
             // 
             this.readonlyUploadExcel.Cursor = System.Windows.Forms.Cursors.Default;
-            this.readonlyUploadExcel.Location = new System.Drawing.Point(88, 332);
+            this.readonlyUploadExcel.Location = new System.Drawing.Point(89, 305);
             this.readonlyUploadExcel.Name = "readonlyUploadExcel";
             this.readonlyUploadExcel.ReadOnly = true;
             this.readonlyUploadExcel.Size = new System.Drawing.Size(592, 20);
             this.readonlyUploadExcel.TabIndex = 3;
-            this.readonlyUploadExcel.GotFocus += readonlyUploadExcel_GotFocus;
             // 
             // btnUploadExcel
             // 
-            this.btnUploadExcel.Location = new System.Drawing.Point(695, 330);
+            this.btnUploadExcel.Location = new System.Drawing.Point(696, 303);
             this.btnUploadExcel.Name = "btnUploadExcel";
             this.btnUploadExcel.Size = new System.Drawing.Size(58, 23);
             this.btnUploadExcel.TabIndex = 4;
@@ -77,7 +85,7 @@
             // comboBoxExcelSheet
             // 
             this.comboBoxExcelSheet.FormattingEnabled = true;
-            this.comboBoxExcelSheet.Location = new System.Drawing.Point(88, 359);
+            this.comboBoxExcelSheet.Location = new System.Drawing.Point(89, 332);
             this.comboBoxExcelSheet.Name = "comboBoxExcelSheet";
             this.comboBoxExcelSheet.Size = new System.Drawing.Size(121, 21);
             this.comboBoxExcelSheet.TabIndex = 5;
@@ -86,11 +94,44 @@
             // lblExcelSheet
             // 
             this.lblExcelSheet.AutoSize = true;
-            this.lblExcelSheet.Location = new System.Drawing.Point(12, 362);
+            this.lblExcelSheet.Location = new System.Drawing.Point(13, 335);
             this.lblExcelSheet.Name = "lblExcelSheet";
             this.lblExcelSheet.Size = new System.Drawing.Size(38, 13);
             this.lblExcelSheet.TabIndex = 6;
             this.lblExcelSheet.Text = "Sheet:";
+            // 
+            // backgroundWorkerTextFileExport
+            // 
+            this.backgroundWorkerTextFileExport.DoWork += new System.ComponentModel.DoWorkEventHandler(this.backgroundWorkerTextFileExport_DoWork);
+            this.backgroundWorkerTextFileExport.RunWorkerCompleted += new System.ComponentModel.RunWorkerCompletedEventHandler(this.backgroundWorkerTextFileExport_RunWorkerCompleted);
+            // 
+            // lblTextFileExport
+            // 
+            this.lblTextFileExport.AutoSize = true;
+            this.lblTextFileExport.Location = new System.Drawing.Point(13, 362);
+            this.lblTextFileExport.Name = "lblTextFileExport";
+            this.lblTextFileExport.Size = new System.Drawing.Size(50, 13);
+            this.lblTextFileExport.TabIndex = 1;
+            this.lblTextFileExport.Text = "Text File:";
+            // 
+            // readonlyTextFileExport
+            // 
+            this.readonlyTextFileExport.Cursor = System.Windows.Forms.Cursors.Default;
+            this.readonlyTextFileExport.Location = new System.Drawing.Point(89, 359);
+            this.readonlyTextFileExport.Name = "readonlyTextFileExport";
+            this.readonlyTextFileExport.ReadOnly = true;
+            this.readonlyTextFileExport.Size = new System.Drawing.Size(592, 20);
+            this.readonlyTextFileExport.TabIndex = 3;
+            // 
+            // btnTextFileExport
+            // 
+            this.btnTextFileExport.Location = new System.Drawing.Point(696, 357);
+            this.btnTextFileExport.Name = "btnTextFileExport";
+            this.btnTextFileExport.Size = new System.Drawing.Size(58, 23);
+            this.btnTextFileExport.TabIndex = 4;
+            this.btnTextFileExport.Text = "...";
+            this.btnTextFileExport.UseVisualStyleBackColor = true;
+            this.btnTextFileExport.Click += new System.EventHandler(this.btnTextFileExport_Click);
             // 
             // Form1
             // 
@@ -99,7 +140,10 @@
             this.ClientSize = new System.Drawing.Size(800, 450);
             this.Controls.Add(this.lblExcelSheet);
             this.Controls.Add(this.comboBoxExcelSheet);
+            this.Controls.Add(this.btnTextFileExport);
             this.Controls.Add(this.btnUploadExcel);
+            this.Controls.Add(this.readonlyTextFileExport);
+            this.Controls.Add(this.lblTextFileExport);
             this.Controls.Add(this.readonlyUploadExcel);
             this.Controls.Add(this.lblUploadExcel);
             this.Controls.Add(this.dataGridViewExcel);
@@ -119,6 +163,10 @@
         private System.Windows.Forms.Button btnUploadExcel;
         private System.Windows.Forms.ComboBox comboBoxExcelSheet;
         private System.Windows.Forms.Label lblExcelSheet;
+        private System.ComponentModel.BackgroundWorker backgroundWorkerTextFileExport;
+        private System.Windows.Forms.Label lblTextFileExport;
+        private System.Windows.Forms.TextBox readonlyTextFileExport;
+        private System.Windows.Forms.Button btnTextFileExport;
     }
 }
 
--- ExcelToTextConvertor/Form1.cs
+++ ExcelToTextConvertor/Form1.cs
@@ -15,7 +15,20 @@ namespace ExcelToTextConvertor
 {
     public partial class Form1 : Form
     {
-        private DataTableCollection dataTableCollection;
+
+        #region Non-Event Handlers e.g. private variables
+
+        private DataTableCollection DataTableCollection { get; set; }
+
+        private struct TxtFileWorkerThreadParams
+        {
+            public string FileName { get; set; }
+            public DataGridViewRowCollection DataGridViewRowCollection { get; set; }
+            public DataGridViewSelectedColumnCollection DataGridViewSelectedColumnCollection { get; set; }
+
+        }
+
+        TxtFileWorkerThreadParams _inputParams;
 
         [DllImport("user32.dll")]
         static extern bool HideCaret(System.IntPtr hWnd);
@@ -24,6 +37,10 @@ namespace ExcelToTextConvertor
             HideCaret(readonlyUploadExcel.Handle);
         }
 
+        #endregion
+
+        #region Form Event Handlers
+
         public Form1()
         {
             InitializeComponent();
@@ -31,10 +48,26 @@ namespace ExcelToTextConvertor
 
         private void comboBoxExcelSheet_SelectedIndexChanged(object sender, EventArgs e)
         {
-            if (dataTableCollection != null && dataTableCollection.Count > 0)
+            if (DataTableCollection != null && DataTableCollection.Count > 0)
             {
-                dataGridViewExcel.DataSource = dataTableCollection[comboBoxExcelSheet.SelectedItem.ToString()];
+                // by default the dgv selection mode is column header select, need to change to row header select or will trigger
+                // invalid operation exception when selecting a worksheet to bind from the uploaded excel file i.e.: 
+                // column's sortmode cannot be set to automatic while the datagridview control's selectionmode is set to columnheaderselect.
+
+                // only upon binding to datasource, then change back selection mode back to column header select in the data source changed event handler
+                dataGridViewExcel.SelectionMode = DataGridViewSelectionMode.RowHeaderSelect;
+                dataGridViewExcel.DataSource = DataTableCollection[comboBoxExcelSheet.SelectedItem.ToString()];
             }
+
+        }
+
+        private void dataGridViewExcel_DataSourceChanged(object sender, EventArgs e)
+        {
+            foreach (DataGridViewColumn column in dataGridViewExcel.Columns)
+            {
+                column.SortMode = DataGridViewColumnSortMode.NotSortable;
+            }
+            dataGridViewExcel.SelectionMode = DataGridViewSelectionMode.ColumnHeaderSelect;
         }
 
         private void btnUploadExcel_Click(object sender, EventArgs e)
@@ -63,12 +96,12 @@ namespace ExcelToTextConvertor
                             });
 
                             // retrieve the dataset's tables using dataset.Tables
-                            dataTableCollection = result.Tables;
+                            DataTableCollection = result.Tables;
 
                             // populate combo box for excel sheets with the excel tables retrieved
                             comboBoxExcelSheet.Items.Clear();
 
-                            foreach (DataTable dt in dataTableCollection)
+                            foreach (DataTable dt in DataTableCollection)
                             {
                                 comboBoxExcelSheet.Items.Add(dt.TableName);
                             }
@@ -83,5 +116,72 @@ namespace ExcelToTextConvertor
         {
             HideCaret();
         }
+
+        private void btnTextFileExport_Click(object sender, EventArgs e)
+        {
+            if (backgroundWorkerTextFileExport.IsBusy) return;
+
+            using (SaveFileDialog saveFileDialog = new SaveFileDialog() { Filter = "Text Documents|*.txt" })
+            {
+                if (saveFileDialog.ShowDialog() == DialogResult.OK
+                    && dataGridViewExcel.SelectedColumns != null
+                    && dataGridViewExcel.SelectedColumns.Count > 0)
+                {
+
+                    readonlyTextFileExport.Text = saveFileDialog.FileName;
+
+                    _inputParams.FileName = saveFileDialog.FileName;
+                    _inputParams.DataGridViewRowCollection = dataGridViewExcel.Rows;
+                    _inputParams.DataGridViewSelectedColumnCollection = dataGridViewExcel.SelectedColumns;
+
+                    backgroundWorkerTextFileExport.RunWorkerAsync(_inputParams);
+                }
+            }
+        }
+
+        private void backgroundWorkerTextFileExport_DoWork(object sender, DoWorkEventArgs e)
+        {
+            DataGridViewRowCollection dataGridViewRowCollection = ((TxtFileWorkerThreadParams)e.Argument).DataGridViewRowCollection;
+            DataGridViewSelectedColumnCollection dataGridViewSelectedColumnCollection = ((TxtFileWorkerThreadParams)e.Argument).DataGridViewSelectedColumnCollection;
+
+            string fileName = ((TxtFileWorkerThreadParams)e.Argument).FileName;
+
+            if (!File.Exists(fileName))
+            {
+                using (File.Create(fileName)) { };
+            }
+
+            using (TextWriter txtWriter = new StreamWriter(new FileStream(fileName, mode: FileMode.Append, access: FileAccess.Write), Encoding.UTF8))
+            {
+                StringBuilder sb = new StringBuilder();
+
+                foreach (DataGridViewRow row in dataGridViewRowCollection)
+                {
+                    for (int colIndex = 0; colIndex < dataGridViewSelectedColumnCollection.Count; colIndex++)
+                    {
+                        sb.Append(row.Cells[dataGridViewSelectedColumnCollection[colIndex].DisplayIndex].Value);
+                        sb.Append("\t");
+                    }
+
+                    sb.AppendLine();
+
+                }
+
+                sb.AppendLine();
+
+                txtWriter.Write(sb.ToString());
+            }
+        }
+
+        private void backgroundWorkerTextFileExport_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
+        {
+            if (e.Error == null)
+            {
+                MessageBox.Show("Your data has been successfully exported");
+            }
+        }
+
+        #endregion
+
     }
 }
